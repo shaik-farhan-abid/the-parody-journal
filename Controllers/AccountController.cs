0181ef115b55d1b82da66b2ec60e8289dee7e2d9
Shaik Farhan Abid <shaikfarhansofficial@gmail.com>
2025-03-06 02:46:32 +0600

    Added Login view, Home Index page, and updated wwwroot assets

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+public class AccountController : Controller
+{
+    // GET: Account/Login
+    public IActionResult Login()
+    {
+        return View();  // It will look for Views/Account/Login.cshtml
+    }
+}
